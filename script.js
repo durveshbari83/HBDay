f2938de15a44f07df3bd113ed3c1130f8cd35027
Durvesh Sandeep Bari <durveshbari83@gmail.com>
2026-06-21 12:07:36 +0530

    Change birthday message to 'Nobody'..

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "to You", "Sneha"],
+    strings: ["HAPPY", "BIRTHDAY!", "to You", "Nobody"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
