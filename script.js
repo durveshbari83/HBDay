2d66a62ba974bddfd89e496306afd01f36d7c5a8
Durvesh Sandeep Bari <durveshbari83@gmail.com>
2026-07-07 21:43:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "to You", "Doctor","Sudarshan"],
+    strings: ["HAPPY", "BIRTHDAY!", "to You", "Katyayani"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
