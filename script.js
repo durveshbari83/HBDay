b382ed84a9b7fc194d74472516e8a675180f9e6c
Durvesh Sandeep Bari <durveshbari83@gmail.com>
2026-07-09 07:57:07 +0530

    Change birthday message from 'Chiku' to 'Jagdish'

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "to You", "Chiku","Don"],
+    strings: ["HAPPY", "BIRTHDAY!", "to You", "Jagdish"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
